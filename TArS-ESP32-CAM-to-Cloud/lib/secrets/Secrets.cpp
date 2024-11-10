d66b979b4d3ff07c6a8ca1c543287299bb7d2b6c
Mikhael Morris Hapataran Siallagan <mikhael.morris@ui.ac.id>
2024-11-10 17:00:27 +0700

    Connect ESP32-CAM to the API links

---
+++ TArS-ESP32-CAM-to-Cloud/lib/secrets/Secrets.cpp
@@ -0,0 +1,4 @@
+#include "Secrets.h"
+
+const char *ssid_name = "POCOs"; // add a name of your choice
+const char *ssid_password = "tes23456"; // add a password of your choice
---
+++ TArS-ESP32-CAM-to-Cloud/lib/secrets/Secrets.h
@@ -0,0 +1,7 @@
+// #ifndef SECRETS
+// #define SECRETS
+
+// extern const char *ssid_name;
+// extern const char *ssid_password;
+
+// #endif
